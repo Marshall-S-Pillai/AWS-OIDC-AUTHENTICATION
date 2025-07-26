fc6d579227800277f29e68929d74612b5e4bbb6f
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2025-07-26 15:02:14 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -5,7 +5,7 @@ sudo apt install -y apache2
 
 # Start and enable the Apache service
 sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo systemctl enable apache2  
 
 # Add a basic index.html
 echo "<h1>Welcome to my Terraform Deployed Web Server!</h1>" | sudo tee /var/www/html/index.html
