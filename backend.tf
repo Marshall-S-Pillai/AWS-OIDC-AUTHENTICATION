b49865ed410a04d621424b9269175fcd742a4f0d
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2025-11-03 21:19:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "powertool19087"
     key            = "env/dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-1" 
     encrypt        = true
     use_lockfile   = true
   }
