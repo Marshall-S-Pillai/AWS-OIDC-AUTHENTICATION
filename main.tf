1fc16265e46a7c80feed506ba75fc6b5d029d4d3
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2026-01-16 00:53:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,67 +13,69 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# --------------------
 # VPC
+# --------------------
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = "Main-VPC"
   }
 }
 
+# --------------------
 # Public Subnet
+# --------------------
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
+
   tags = {
     Name = "Public-Subnet"
   }
 }
 
-# Private Subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "Private-Subnet"
-  }
-}
-
+# --------------------
 # Internet Gateway
+# --------------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+
   tags = {
     Name = "Internet-Gateway"
   }
 }
 
-# Public Route Table
+# --------------------
+# Route Table
+# --------------------
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
+
   tags = {
     Name = "Public-Route-Table"
   }
 }
 
-# Route Table Associations
-resource "aws_route_table_association" "public_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# Public Route
 resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# --------------------
 # Security Group
+# --------------------
 resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.main.id
 
@@ -96,13 +98,19 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+# --------------------
 # EC2 Instance (Web Server)
+# --------------------
 resource "aws_instance" "web_server" {
   ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   user_data              = file("${path.module}/userdata.sh")
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = {
     Name = "Web-Server"
