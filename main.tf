98807f041908338f1702fdc6fb6fc449d8306aab
Marshall <143778910+Marshall-S-Pillai@users.noreply.github.com>
2026-01-16 00:33:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "aws_security_group" "web_sg" {
 # EC2 Instance (Web Server)
 resource "aws_instance" "web_server" {
   ami                    = "ami-0c02fb55956c7d316"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   user_data              = file("${path.module}/userdata.sh")
